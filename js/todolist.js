9a3e2f34719dfbe941c1c922361136e51dc7e5c9
unknown <achange888@gmail.com>
2021-12-16 19:33:27 +0900

    Saving To Dos

--- js/todolist.js
+++ js/todolist.js
@@ -2,11 +2,27 @@ const toDoForm = document.querySelector(".todo-form");
 const toDoInput = document.querySelector(".todo-form input");
 const toDolist = document.querySelector(".todo-list");
 
+const toDos = [];
+
+function saveToDos() {
+    localStorage.setItem("todos",JSON.stringify(toDos));
+}
+
+
+function deleteToDo(event) {
+    const list = event.target.parentElement;
+    list.remove();
+  }
+
 function paintToDo (newTodo){ 
  const li = document.createElement("li");
  const span = document.createElement("span");
+ span.innerText = newTodo;
+ const button = document.createElement("button");
+ button.innerText = "X";
+ button.addEventListener("click",deleteToDo);
  li.appendChild(span);
- span.innerText = newTodo;3.
+ li.appendChild(button);
  toDolist.appendChild(li);
 }
 
@@ -14,7 +30,11 @@ function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    toDos.push(newTodo);
     paintToDo(newTodo);
+    saveToDos();
 }
+
+
 
 toDoForm.addEventListener("submit",handleToDoSubmit)
